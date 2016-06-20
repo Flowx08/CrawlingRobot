577911ae2680d60b4d1b74bd4e4abe6dea9dd3f0
Carlo Meroni <meroni_carlo@yahoo.it>
2016-06-20 21:01:17 +0200

    First commit

---
+++ main.c
@@ -0,0 +1,53 @@
+////////////////////////////////////////////////////////////
+///	INCLUDES
+////////////////////////////////////////////////////////////
+#include <stdio.h>
+#include <wiringPi.h>
+#include "qlearning.h"
+
+/*
+	-- Raspberry Pi B+ Pinout table
+	Link: https://it.pinout.xyz/pinout/pin17_alimentazione_a_33v
+	
+	*Left type*			*BCM pin*	*WiringPi pin*	*Right type*
+
+	Alimentazione 3.3V		1,2			_,_			Alimentazione 5V
+	BCM 2 (SDA)				3,4			8,_			Alimentazione 5V
+	BCM 3 (SCL)				5,6			9,_			Massa
+	BCM 4 (GPCLK0)			7,8			7,15		BCM 14 (TXD)
+	Massa					9,10		_,16		BCM 15 (RXD)
+	BCM 17					11,12		0,1			BCM 18 (PCM_C)
+	BCM 27 (PCM_D)			13,14		2,_			Massa
+	BCM 22					15,16		3,4			BCM 23
+	Alimentazione 3.3V		17,18		_,5			BCM 24
+	BCM 10 (MOSI)			19,20		12,_		Massa
+	BCM 9 (MISO)			21,22		13,6		BCM 25
+	BCM 11 (SCLK)			23,24		14,10		BCM 8 (CE0)
+	Massa					25,26		_,11		BCM 7 (CE1)
+	
+	BCM 0 (ID_SD)			27,28		30,31		BCM 1 (ID_SC)
+	BCM 5					29,30		21,_		Massa
+	BCM 6					31,32		22,26		BCM 12
+	BCM 13					33,34		23,_		Massa
+	BCM 19 (MISO)			35,36		24,27		BCM 16
+	BCM 26					37,38		25,28		BCM 20 (MOSI)
+	Massa					39,40		_,29		BCM 21 (SCLK)
+*/
+
+int main(int argc, const char *argv[])
+{
+	printf("Hello World\n");
+
+	wiringPiSetup();
+	pinMode(27, OUTPUT);
+	
+	for (;;)
+	{
+		digitalWrite(27, HIGH);
+		delay(1000);
+		digitalWrite(27, LOW);
+		delay(1000);
+	}
+
+	return 0;
+}
---
+++ qlearning.h
@@ -0,0 +1,118 @@
+////////////////////////////////////////////////////////////
+///	INCLUDES
+////////////////////////////////////////////////////////////
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//Random number between 0 and 1
+#define RANDF() (double)rand() / RAND_MAX 
+
+typedef struct Qstruct
+{
+	float **table;
+	int states_count;
+	int actions_count;
+	int state;
+	int action;
+	float learning_rate;
+	float longterm_learning;
+        float exploration_factor;
+} Qstruct;
+
+////////////////////////////////////////////////////////////
+/// \brief	Create a Qstruct	
+///
+////////////////////////////////////////////////////////////
+Qstruct* qstruct_init(int states_count, int actions_count)
+{
+	Qstruct* q = (Qstruct*)malloc(sizeof(Qstruct));
+	q->table = (float**)malloc(sizeof(float*) * states_count);
+	for (int i = 0; i < states_count; i++) {
+		q->table[i] = (float*)malloc(sizeof(float) * actions_count);
+		for (int k = 0; k < actions_count; k++)
+			q->table[i][k] = 0;
+	}
+	q->states_count = states_count;
+	q->actions_count = actions_count;
+	q->learning_rate = 0.5;
+	q->longterm_learning = 0.5;
+        q->exploration_factor = 0.1;
+	return q;
+}
+
+////////////////////////////////////////////////////////////
+/// \brief	Destroy a Qstruct	
+///
+////////////////////////////////////////////////////////////
+void qstruct_free(Qstruct* q)
+{
+	for (int i = 0; i < q->states_count; i++)
+		free(q->table[i]);
+	free(q->table);
+	free(q);
+}
+
+////////////////////////////////////////////////////////////
+/// \brief	Return an action to perform based on the current
+/// world state
+///
+////////////////////////////////////////////////////////////
+int qstruct_takeaction(Qstruct* q, int state)
+{
+	//Exploration - Exploitation control 
+	if (RANDF() < q->exploration_factor) {
+                q->state = state;
+		q->action = rand() % q->actions_count;
+		return q->action;
+	}
+
+	//Get best action id and value
+	double rmax = -0x7FFF;
+	int maxid = 0;
+	for (int i = 0; i < q->actions_count; i++)
+		if (q->table[state][i] > rmax) {
+			rmax = q->table[state][i];
+			maxid = i;
+		}
+
+	//Select the best action as current action
+	q->action = maxid;
+
+	//Store current state for later
+	q->state = state;
+
+	return q->action;
+}
+
+////////////////////////////////////////////////////////////
+/// \brief	Get maximum reward value at a particular state	
+///
+////////////////////////////////////////////////////////////
+double qstruct_getmaxreward(Qstruct* q, int state)
+{
+	//Get max reward value at a particular state
+	float rmax = -0x7FFF;
+	for (int i = 0; i < q->actions_count; i++)
+		if (q->table[state][i] > rmax) rmax = q->table[state][i];
+	return rmax;
+}
+
+////////////////////////////////////////////////////////////
+/// \brief	Update that Q matrix	
+///
+////////////////////////////////////////////////////////////
+void qstruct_update(Qstruct* q, int state_new, float reward)
+{
+	//Update the Q matrix with based on world changes and reward feedbacks
+	//This is the Qleaning rule
+	q->table[q->state][q->action] += q->learning_rate * (reward +  
+		q->longterm_learning * qstruct_getmaxreward(q, state_new) - q->table[q->state][q->action]);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
